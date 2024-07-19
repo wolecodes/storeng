6b2cd544a341e5d938dbcdc00b0e5cfcd7640c31
oyeyemi toheeb <opeyemioyeyemi141@gmail.com>
2024-07-19 23:19:41 +0100

    created create app, added tailwin, import react dom, created the routes,compoents and pages

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+/**
+ * Functional component representing the footer of the application.
+ * @returns {JSX.Element} Footer component
+ */
+
+const Footer = () => {
+  return <div>Footer</div>;
+};
+
+export default Footer;
